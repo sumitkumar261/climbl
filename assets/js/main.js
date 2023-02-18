991ac69ead6690b7194f56557c4d712231b4866a
Tushar Rao <tushar.rao198@gmail.com>
2023-02-18 05:37:39 +0530

    major changes - climbl

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**
-* Template Name: Impact - v1.2.0
-* Template URL: https://bootstrapmade.com/impact-bootstrap-business-website-template/
+* Template Name: Climbl - v1.2.0
+* Template URL: https://bootstrapmade.com/Climbl-bootstrap-business-website-template/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
